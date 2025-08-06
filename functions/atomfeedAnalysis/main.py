8e155114977c69bfc03fdc56b77a824c77d6b736
WhineyMonkey10 <gbialystokjoly@gmail.com>
2025-08-06 06:55:17 -0400

    changed it back to a limit of 5 entires

--- functions/atomfeedAnalysis/main.py
+++ functions/atomfeedAnalysis/main.py
@@ -58,7 +58,7 @@ def main(context):
     
     databases = Databases(client)
     
-    for entry in feed.entries[:10]:
+    for entry in feed.entries[:5]:
         api_url = f"https://api.github.com/repos/appwrite/appwrite/commits/{extract_sha(entry.link)}"
         response = requests.get(api_url, headers=headers)
         if response.status_code == 200:
