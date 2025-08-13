09b116e223ed7de6d7d9d8ca49182dab28bf7f8c
WhineyMonkey10 <gbialystokjoly@gmail.com>
2025-08-13 10:34:44 -0400

    serve html using js instead of node

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname)));
+
+// Route for the home page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Route for the delete page
+app.get('/delete', (req, res) => {
+  res.sendFile(path.join(__dirname, 'delete.html'));
+});
+
+// Fallback route - send to index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
